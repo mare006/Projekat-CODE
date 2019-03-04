b1490539579a27454b29ed527f2812361f9ac2b0
mare006 <mare006@google.com>
2019-03-04 05:30:23 +0100

    left and mid animations finished

--- js/main.js
+++ js/main.js
@@ -1,84 +1,79 @@
-// function toggleDivBtn() {
-//     var x = document.getElementById("subBtns");
-//     if (x.style.display === "none") {
-//         x.style.display = "block";
-//     } else {
-//         x.style.display = "none";
-//     }
-// }
-//
-///main btns toggle on #Bio hide on !#Bio
-//radi toggle al neće fade i vreme itt
+///toggle content
+//za copy/paste da ne kucam 100puta
+// #Bio,#RatStruja,#Drustvo,#Pronalasci,#BioRaneGod,#BioSrGod,#BioZlGod,#BioPozGod
+// .BioContent,.RatStrujaContent,.DrustvoContent,.PronalasciContent,.BioRaneGodContent,.BioSrGodContent,.BioZlGodContent,.BioPozGodContent
+
+
+///toggle content
+///subBtns jq animated in main bar on Bio btn
 $(document).ready(function () {
-    $("#Bio").click(function () {
-        $("#BioRaneGod").toggle();
-        $("#BioSrGod").toggle();
-        $("#BioZlGod").toggle();
-        $("#BioPozGod").toggle();
+    $("div#Bio").mouseenter(function () {
+        $("div#subBtns").show(400, function () {
+            $("div#BioRaneGod").slideDown(1000);
+            $("div#BioSrGod").delay(100).slideDown(800);
+            $("div#BioZlGod").delay(200).slideDown(600);
+            $("div#BioInteraktivna").delay(300).slideDown(400);
+            if ($("div#BioRaneGod").css("display") == "block") {
+                $("div#subBtns p").show(200);
+            } else $("div#subBtns p").delay(310).show(200);
+        });
     });
-    $("#RatStruja,#Drustvo,#Pronalasci").click(function () {
-        $("#BioRaneGod").hide();
-        $("#BioSrGod").hide();
-        $("#BioZlGod").hide();
-        $("#BioPozGod").hide();
+    $("#leftContentMenu").mouseleave(function () {
+        $("div#subBtns p").hide(200);
+        $("div#subBtns").delay(200).hide(400);
     });
 });
-///toggle content
+///Main content jq animations show/hide and auto-scroll on btn clicks
+//Bio hide all main content
 $(document).ready(function () {
-        //mainBtns
-        $("#Bio").click(function () {
-        $(".BioContent").toggle();
-        });
-        $("#RatStruja").click(function () {
-        $(".RatStrujaContent").toggle();
-        });
-        $("#Drustvo").click(function () {
-        $(".DrustvoContent").toggle();
-        });
-        $("#Pronalasci").click(function () {
-        $(".PronalasciContent").toggle();
-        });
-        //subBtns
-        $("#BioRaneGod").click(function () {
-        $(".BioRaneGodContent").toggle();
-        });
-        $("#BioSrGod").click(function () {
-        $(".BioSrGodContent").toggle();
-        });
-        $("#BioZlGod").click(function () {
-        $(".BioZlGodContent").toggle();
-        });
-        $("#BioPozGod").click(function () {
-        $(".BioPozGodContent").toggle();
-        });
-        //mainBtns
-        $("#RatStruja,#Drustvo,#Pronalasci,#BioRaneGod,#BioSrGod,#BioZlGod,#BioPozGod").click(function () {
-            $(".BioContent").hide();
-        });
-        $("#Bio,#Drustvo,#Pronalasci,#BioRaneGod,#BioSrGod,#BioZlGod,#BioPozGod").click(function () {
-            $(".RatStrujaContent").hide();
-        });
-        $("#Bio,#RatStruja,#Pronalasci,#BioRaneGod,#BioSrGod,#BioZlGod,#BioPozGod").click(function () {
-            $(".DrustvoContent").hide();
-        });
-        $("#Bio,#RatStruja,#Drustvo,#BioRaneGod,#BioSrGod,#BioZlGod,#BioPozGod").click(function () {
-            $(".PronalasciContent").hide();
-        });
-        //subBtns
-        $("#Bio,#RatStruja,#Drustvo,#Pronalasci,#BioSrGod,#BioZlGod,#BioPozGod").click(function () {
-            $(".BioRaneGodContent").hide();
-        });
-        $("#Bio,#RatStruja,#Drustvo,#Pronalasci,#BioRaneGod,#BioZlGod,#BioPozGod").click(function () {
-            $(".BioSrGodContent").hide();
-        });
-        $("#Bio,#RatStruja,#Drustvo,#Pronalasci,#BioRaneGod,#BioSrGod,#BioPozGod").click(function () {
-            $(".BioZlGodContent").hide();
-        });
-        $("#Bio,#RatStruja,#Drustvo,#Pronalasci,#BioRaneGod,#BioSrGod,#BioZlGod").click(function () {
-            $(".BioPozGodContent").hide();
-        });
+    $("#Bio").click(function () {
+        $(".mainContent").fadeOut(400);
 
     });
-    //za copy/paste da ne kucam 100puta
-    // #Bio,#RatStruja,#Drustvo,#Pronalasci,#BioRaneGod,#BioSrGod,#BioZlGod,#BioPozGod
-    // .BioContent,.RatStrujaContent,.DrustvoContent,.PronalasciContent,.BioRaneGodContent,.BioSrGodContent,.BioZlGodContent,.BioPozGodContent
+});
+//Other btns(!Bio) show/hide and autoscroll
+function BtnOnClickContent(idBtn, idContent) {
+    $(idBtn).click(function () {
+        $("#centralContentMain").animate({
+            scrollTop: 0
+        }, 300, 'swing');
+        $(".mainContent").delay(300).fadeOut(300, function () {
+            $(idContent).delay(300).fadeIn(300);
+        });
+    });
+}
+$(document).ready(function () {
+    BtnOnClickContent("#BioRaneGod", "#BioRaneGodContent");
+    BtnOnClickContent("#BioSrGod", "#BioSrGodContent");
+    BtnOnClickContent("#BioZlGod", "#BioZlGodContent");
+    BtnOnClickContent("#BioPozGod", "#BioPozGodContent");
+    BtnOnClickContent("#BioInteraktivna", "#BioInteraktivnaContent");
+    BtnOnClickContent("#RatStruja", "#RatStrujaContent");
+    BtnOnClickContent("#Pronalasci", "#PronalasciContent");
+    BtnOnClickContent("#Zanimljivosti", "#ZanimljivostiContent");
+});
+
+
+//ovo isto radi al kod lošiji
+// $(document).ready(function () {
+//     $("#BioRaneGod").click(function () {
+//         $("#centralContentMain").animate({scrollTop:0}, 500, 'swing');
+//         if ($(".subContent,.mainContent").not("#BioRaneGodContent").css("display")=="block") {
+//             $(".subContent,.mainContent").not("#BioRaneGodContent").fadeOut(400);
+//         }
+//         if ($("#BioRaneGodContent").css("display") == "none") {
+//             $("#BioRaneGodContent").fadeIn(400);
+//         }
+//     });
+// });
+// $(document).ready(function () {
+//     $("#BioSrGod").click(function () {
+//         $("#centralContentMain").animate({scrollTop:0}, 500, 'swing');
+//         if ($(".subContent,.mainContent").not("#BioSrGodContent").css("display")=="block") {
+//             $(".subContent,.mainContent").not("#BioSrGodContent").fadeOut(400);
+//         }
+//         if ($("#BioSrGodContent").css("display") == "none") {
+//             $("#BioSrGodContent").fadeIn(400);
+//         }
+//     });
+// });
